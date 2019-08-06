e5af71f7b00d822c6202908ec7b55310332afd9e
Alex Coupe <Alex.Coupe@slis.local>
2019-08-06 17:04:11 +0100

    Navbar looking better

--- src/Components/Core/NavBar.js
+++ src/Components/Core/NavBar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar() {
     return (
@@ -6,26 +8,12 @@ export default function NavBar() {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand text-white" >New SLIM</a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item nav-link active">
+                      <h3>Welcome, Username</h3>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                       <a className="nav-link" href="#"> <FontAwesomeIcon icon={faCog} size="2x" /> </a>
                     </li>
                     </ul>
                 </div>
