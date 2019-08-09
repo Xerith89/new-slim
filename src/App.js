d8b638d3e46bb1fbb55d4a9752c790c3dc32299b
Alex Coupe <Alex.Coupe@slis.local>
2019-08-09 14:35:43 +0100

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Dashboard from './Components/Meta/Dashboard'
 import TopBar from './Components/Core/TopBar/TopBar'
+import Footer from './Components/Core/Footer/Footer'
 
 function App() {
     return ( 
     <div className = "App">
         <TopBar user="Alex"/>
         <Dashboard />
+        <Footer />
     </div>
     );
 }
