d065b6f3be2a3e14efd9f5b454d9f1fa490a89e1
Alex Coupe <acoupe@gmail.com>
2019-08-09 21:52:32 +0100

    Properly nested component

--- src/Components/Meta/Dashboard.js
+++ src/Components/Meta/Dashboard.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 import NavColumn from '../Core/NavCol/NavColumn'
 import DiaryReviews from '../Core/DiaryReviews'
 import MessageReview from '../Core/MessageReview'
+import Breadcrumb from '../Core/Breadcrumb'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className = "Dashboard">
-                <div className="row mt-5">
+                <Breadcrumb title="Dashboard"/>
+                <div className="row mr-0">
+                    <div className="col-md-3">
                         <NavColumn />
+                    </div>
+                    <div className="col-md-9">
                         <DiaryReviews />
-                    <div className="col-12">
                         <MessageReview />
                     </div>
                 </div>
