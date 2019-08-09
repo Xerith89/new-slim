0d85a27beba4e7ed05d5ff7ff2dbaf308c10e699
Alex Coupe <Alex.Coupe@slis.local>
2019-08-09 16:34:10 +0100

    Diary reviews are in

--- src/Components/Core/DiaryReviews.js
+++ src/Components/Core/DiaryReviews.js
@@ -5,18 +5,16 @@ export default class DiaryReviews extends Component {
     render() {
 
         const style = {
-            width: "100%"
+           
         }
         return (
             <div>
-                <div className="col-md-12">
-                    <div id="overview" className="card" style={style}>
-                        <h5 className="card-header main-color-bg">Search</h5>
+                <div className="col-md-12 mt-5 ml-5">
+                    <div id="overview" className="card" style={{ width: "30rem"}}>
+                        <h5 className="card-header main-color-bg">My Diary Reviews</h5>
                         <div className="card-body">
-                            <div className="card-deck">
-                                <div className="card">
+                            <div className="card">
 
-                                </div>
                             </div>
                         </div>
                     </div> 
--- src/Components/Meta/Dashboard.js
+++ src/Components/Meta/Dashboard.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import NavColumn from '../Core/NavColumn'
+import NavColumn from '../Core/NavCol/NavColumn'
+import DiaryReviews from '../Core/DiaryReviews'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className = "Dashboard">
-                <div className="row">
-                   
-                    <div className="ml-3">
-                    </div>
+                <div className="row mt-5">
+                    <NavColumn />
+                    <DiaryReviews />
                 </div>
             </div>
         )
