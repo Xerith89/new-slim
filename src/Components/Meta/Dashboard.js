dac8772b81ae6a8e5eba4730fe25c169fbb1ad4a
Alex Coupe <Alex.Coupe@slis.local>
2019-08-09 13:47:05 +0100

    NavCol removed for now

--- src/Components/Meta/Dashboard.js
+++ src/Components/Meta/Dashboard.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import NavColumn from '../Core/NavColumn'
-import SearchArea from '../Core/SearchArea'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className = "Dashboard">
                 <div className="row">
-                    <NavColumn />
+                   
                     <div className="ml-3">
                     </div>
                 </div>
