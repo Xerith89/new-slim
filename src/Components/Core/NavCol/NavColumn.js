021081dffdb1f230a4042689d3927efaefe8ef76
Alex Coupe <Alex.Coupe@slis.local>
2019-08-09 16:32:13 +0100

    Switch around

---
+++ src/Components/Core/NavCol/NavColumn.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import NavItem from './NavItem'
+
+// This component renders on the left of the screen and provides navigation links for the app
+
+export default class NavColumn extends Component {
+
+
+    render() {
+        
+    //This stores the links for the claims section of the Nav
+    const claimsLinks = {
+        links: [
+            "New Claim",
+            "Diary Reviews",
+            "Reports",
+            "Another Link"
+        ]
+    }
+
+    const accountsLinks = {
+        links: [
+            "Direct Debits",
+            "Other Accounts Stuff",
+            "Another Link",
+            "Another Link"
+        ]
+    }
+
+    const underwritingLinks = {
+        links: [
+            "New Quote",
+            "Renewals",
+            "Another Link",
+            "Another Link"
+        ]
+    }
+
+    const agencyLinks = {
+        links: [
+            "Manage Brokers",
+            "Other Accounts Stuff",
+            "Another Link",
+            "Another Link"
+        ]
+    }
+        return (
+            <div>
+                <div id="navMenu" data-testid="navcol" className="card text-center ml-2 mt-5 mb-5" style={{ width: '18rem'}}>
+                    <div className="card-body p-0">
+                        <NavItem title="Claims" links={claimsLinks.links} />
+                        <NavItem title="Underwriting" links={underwritingLinks.links} />
+                        <NavItem title="Accounts" links={accountsLinks.links} />
+                        <NavItem title="Agency" links={agencyLinks.links} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Core/NavCol/NavItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavItem(props) {
+    return (
+        <div>
+            <button className="btn btn-block" style={{backgroundColor: "#ebe7dd"}}><strong>{props.title}</strong></button>
+            {props.links.map((element, i) => {
+                return ( <React.Fragment key={i}>
+                <button className=" btn btn-block btn-dark m-0 " >{element}</button>
+                <hr style={{margin: '0rem', borderColor: '#202020'}}/>
+                </React.Fragment>
+                )
+            })}
+        </div>
+    )
+}
