03d2b2ad04dc8937057559115b71a9bd286b2b4c
Alex Coupe <Alex.Coupe@slis.local>
2019-08-13 16:34:07 +0100

    Style

--- src/Components/Core/NavCol/NavItem.js
+++ src/Components/Core/NavCol/NavItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavItem(props) {
     return (
         <div>
-            <button className="btn btn-block" style={{backgroundColor: "#ebe7dd"}}><strong>{props.title}</strong></button>
+            <button className="btn btn-block bg-light" style={{backgroundColor: "#ebe7dd"}}><strong>{props.title}</strong></button>
             {props.links.map((element, i) => {
                 return ( <React.Fragment key={i}>
                 <button className=" btn btn-block btn-dark m-0 " >{element}</button>
