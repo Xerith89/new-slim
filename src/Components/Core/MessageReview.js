ac60634d95df9ddaa44d0ba71c373dbd360401e4
Alex Coupe <Alex.Coupe@slis.local>
2019-08-14 14:45:36 +0100

    Adding table Data

--- src/Components/Core/MessageReview.js
+++ src/Components/Core/MessageReview.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import FilterBar from './FilterBar';
+import FilterBar from './FilterBar'
+import TableDisplayData from './TableDisplayData'
 
 export default class MessageReview extends Component {
 
@@ -49,38 +50,6 @@ export default class MessageReview extends Component {
                         <h5 className="card-header text-center" style={{backgroundColor: "#ebf2fc"}}>Messages <span className="badge badge-dark">3</span></h5>
                         <div className="card-body">
                            <FilterBar options={filterOptions} />
-                            <div className="m-3">
-                                <table className="table table-striped table-hover">
-                                    <thead>
-                                        <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Date</th>
-                                        <th scope="col">Subject</th>
-                                        <th scope="col"></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                        <td>Mark Otto</td>
-                                        <td>12-04-19</td>
-                                        <td>Callback</td>
-                                        <td></td>
-                                        </tr>
-                                        <tr>
-                                        <td>Jacob Thornton</td>
-                                        <td>14-04-19</td>
-                                        <td>New Claim</td>
-                                        <td></td>
-                                        </tr>
-                                        <tr>
-                                        <td>Larry</td>
-                                        <td>Bird</td>
-                                        <td>18-04-19</td>
-                                        <td></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
                         </div>
                     </div> 
                 </div>
