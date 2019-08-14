ab678a7dfa55b1ca0285759316f53f596d105113
Alex Coupe <Alex.Coupe@slis.local>
2019-08-14 15:02:31 +0100

    Getting some placeholde API data

--- src/Components/Core/TopBar/SearchResults.js
+++ src/Components/Core/TopBar/SearchResults.js
@@ -9,7 +9,7 @@ import {
 export default class SearchResults extends Component {
     state = {
         modal:false,
-        name: ''
+        searchResults: []
     }
 
     toggle = () => {
@@ -18,14 +18,32 @@ export default class SearchResults extends Component {
         })
     }
 
+    fetchResults = () => {
+        fetch('http://jsonplaceholder.typicode.com/posts?_limit=5')
+        .then(response => response.json())
+        .then(json => this.setState({
+            searchResults: json
+        }))
+        .catch(error => console.log(error));
+    }
+
+    handleClick = () => {
+        this.toggle();
+        this.fetchResults();
+    }
+
     render() {
         return (
             <div>
-                <Button color="dark" className="btn-block" data-testid="searchButton" onClick={this.toggle}>Search</Button>
-
+                <Button color="dark" className="btn-block" data-testid="searchButton" onClick={this.handleClick}>Search</Button>
                 <Modal data-testid="searchResults" isOpen={this.state.modal} toggle={this.toggle}>
                     <ModalHeader toggle={this.toggle}>Search Results</ModalHeader>
                     <ModalBody>
+                        {this.state.searchResults.map((result) => {
+                            return (
+                                <h5>{result.title}</h5>
+                            )
+                        })}
                     </ModalBody>
                 </Modal>
             </div>
