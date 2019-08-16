4a0202ff939a0c6e7eeec9f290090712efc8c64d
Alex Coupe <Alex.Coupe@slis.local>
2019-08-16 13:38:47 +0100

    Color scheme change

--- src/Components/Core/NavCol/NavBlock.js
+++ src/Components/Core/NavCol/NavBlock.js
@@ -21,7 +21,7 @@ export default class NavBlock extends Component {
     render() {
         return (
             <div>
-                <button className="btn btn-block bg-light border" data-testid="navButton" style={{backgroundColor: "#ebe7dd", borderColor: "#202020"}} onClick={this.handleClick}><strong>{this.props.title}</strong></button>
+                <button className="btn btn-block border" data-testid="navButton" style={{backgroundColor: "#dfe4e6", borderColor: "#868786"}} onClick={this.handleClick}><strong>{this.props.title}</strong></button>
                 <SlideDown className={'my-dropdown-slidedown'}>
                     {this.state.childrenVisible ? <NavItem links={this.props.links}/> : null}
                 </SlideDown>
