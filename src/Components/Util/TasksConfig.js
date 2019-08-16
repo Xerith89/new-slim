f0aed19f0479b99a9b60f69ebdb0ba999b83c6d0
Alex Coupe <Alex.Coupe@slis.local>
2019-08-16 15:27:28 +0100

    Options in a separate file

---
+++ src/Components/Util/TasksConfig.js
@@ -0,0 +1,44 @@
+const filterOptions = 
+[
+    {
+        header: 'Tasks',
+        body: [
+            'My Tasks',
+            'Team Tasks'
+        ]
+    },
+
+    {
+        header: 'Status',
+        body: [
+            'Reviewed',
+            'Unreviewed'
+        ]
+    },
+
+    {
+        header: 'Priority',
+        body: [
+            'Normal',
+            'Urgent'
+        ]
+    },
+    {
+        header: 'Date',
+        body: [
+            'Last Week',
+            'Last Two Weeks',
+            'Last Month',
+            'Custom'
+        ]
+    },
+]
+
+const tableOptions = [
+    'Task Name', 'Raised On', 'Type', 'Assigned To', 'Priority', 'Due Date'
+]
+
+module.exports = {
+    filterOptions:filterOptions,
+    tableOptions:tableOptions
+}
