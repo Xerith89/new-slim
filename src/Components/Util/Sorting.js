8de8c72a02ac370dfb907d0bce012f59b9b2ba5e
Alex Coupe <acoupe@gmail.com>
2019-08-18 14:58:10 +0100

    Sorting fixed and tested

--- src/Components/Util/Sorting.js
+++ src/Components/Util/Sorting.js
@@ -2,20 +2,29 @@ function sortAscending(inputObjectArray, sortProperty) {
     inputObjectArray.sort((a,b) => {
         const aUpper = a[sortProperty].toUpperCase();
         const bUpper = b[sortProperty].toUpperCase();
+       
         let comparison = 0;
         if (aUpper < bUpper) {
             comparison = 1;
             } else if (aUpper > bUpper) {
             comparison = -1;
             }
+            
         return comparison;
     });
 }
 
 function sortDescending(inputObjectArray, sortProperty) { 
     inputObjectArray.sort((a,b) => {
-        const aUpper = a[sortProperty].toUpperCase();
-        const bUpper = b[sortProperty].toUpperCase();
+        let aUpper;
+        let bUpper;
+        if (typeof(a[sortProperty] === 'string')) { 
+            aUpper = a[sortProperty].toUpperCase();
+            bUpper = b[sortProperty].toUpperCase();
+        } else {
+            aUpper = a[sortProperty];
+            bUpper = b[sortProperty];
+        }
         let comparison = 0;
         if (aUpper > bUpper) {
             comparison = 1;
--- src/Tests/UtilTests/Sort.test.js
+++ src/Tests/UtilTests/Sort.test.js
@@ -1,34 +1,53 @@
 import Sorting from '../../Components/Util/Sorting'
 
-test('SortByString sorts ascending', () => {
-    let objectsToSort = [
-        {
-            id: 1,
-            taskName: 'Review New Claim',
-            claimSpecNo: 5500001,
-            type: 'Claim', 
-            assignedTo: 'Alex Coupe',
-            priority: 'Normal',
-            dueDate: '01-09-2019'
-        },
-        {
-            id: 2,
-            taskName: 'Review New Invoice',
-            claimSpecNo: 5500000,
-            type: 'Claim', 
-            assignedTo: 'Alex Coupe',
-            priority: 'Normal',
-            dueDate: '01-09-2019'
-        },
-       
-    ]
+
+const objectsToSort = [
+    {
+        id: 1,
+        taskName: 'Review New Claim',
+        claimSpecNo: '5500001',
+        type: 'Claim', 
+        assignedTo: 'Alex Coupe',
+        priority: 'Normal',
+        dueDate: '01-09-2019'
+    },
+    {
+        id: 2,
+        taskName: 'Review New Invoice',
+        claimSpecNo: '5500000',
+        type: 'Claim', 
+        assignedTo: 'Alex Coupe',
+        priority: 'Normal',
+        dueDate: '01-09-2019'
+    },
+    {
+        id: 3,
+        taskName: 'Diary Review',
+        claimSpecNo: '5500244',
+        type: 'Claim', 
+        assignedTo: 'Donald Duck',
+        priority: 'Urgent',
+        dueDate: '29-09-2019'
+    },
+    {
+        id: 4,
+        taskName: 'Loss Adjustor Report Received',
+        claimSpecNo: '5500044',
+        type: 'Claim', 
+        assignedTo: 'Peter Parker',
+        priority: 'Urgent',
+        dueDate: '05-09-2019'
+    },
+]
+
+test('Sorts Ascending By Input Property', () => {
     let sorted = objectsToSort;
     Sorting.sortAscending(sorted, 'taskName');
     expect(sorted).toEqual([
         {
             id: 2,
             taskName: 'Review New Invoice',
-            claimSpecNo: 5500000,
+            claimSpecNo: '5500000',
             type: 'Claim', 
             assignedTo: 'Alex Coupe',
             priority: 'Normal',
@@ -37,44 +56,59 @@ test('SortByString sorts ascending', () => {
         {
             id: 1,
             taskName: 'Review New Claim',
-            claimSpecNo: 5500001,
+            claimSpecNo: '5500001',
             type: 'Claim', 
             assignedTo: 'Alex Coupe',
             priority: 'Normal',
             dueDate: '01-09-2019'
+        },
+        {
+            id: 4,
+            taskName: 'Loss Adjustor Report Received',
+            claimSpecNo: '5500044',
+            type: 'Claim', 
+            assignedTo: 'Peter Parker',
+            priority: 'Urgent',
+            dueDate: '05-09-2019'
+        },
+        {
+            id: 3,
+            taskName: 'Diary Review',
+            claimSpecNo: '5500244',
+            type: 'Claim', 
+            assignedTo: 'Donald Duck',
+            priority: 'Urgent',
+            dueDate: '29-09-2019'
         },
     ]);
 });
 
-test('SortByString sorts descending', () => {
-    let objectsToSort = [
-       
-        {
-            id: 2,
-            taskName: 'Review New Invoice',
-            claimSpecNo: 5500000,
-            type: 'Claim', 
-            assignedTo: 'Alex Coupe',
-            priority: 'Normal',
-            dueDate: '01-09-2019'
-        },
-        {
-            id: 1,
-            taskName: 'Review New Claim',
-            claimSpecNo: 5500001,
-            type: 'Claim', 
-            assignedTo: 'Alex Coupe',
-            priority: 'Normal',
-            dueDate: '01-09-2019'
-        },
-    ];
+test('Sort Descending By Input Property', () => {
     let sorted = objectsToSort;
     sorted.sort(Sorting.sortDescending(sorted, 'taskName'));
     expect(sorted).toEqual([      
         {
+            id: 3,
+            taskName: 'Diary Review',
+            claimSpecNo: '5500244',
+            type: 'Claim', 
+            assignedTo: 'Donald Duck',
+            priority: 'Urgent',
+            dueDate: '29-09-2019'
+        },
+        {
+            id: 4,
+            taskName: 'Loss Adjustor Report Received',
+            claimSpecNo: '5500044',
+            type: 'Claim', 
+            assignedTo: 'Peter Parker',
+            priority: 'Urgent',
+            dueDate: '05-09-2019'
+        },
+        {
             id: 1,
             taskName: 'Review New Claim',
-            claimSpecNo: 5500001,
+            claimSpecNo: '5500001',
             type: 'Claim', 
             assignedTo: 'Alex Coupe',
             priority: 'Normal',
@@ -83,7 +117,7 @@ test('SortByString sorts descending', () => {
         {
             id: 2,
             taskName: 'Review New Invoice',
-            claimSpecNo: 5500000,
+            claimSpecNo: '5500000',
             type: 'Claim', 
             assignedTo: 'Alex Coupe',
             priority: 'Normal',
