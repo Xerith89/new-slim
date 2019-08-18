55715e389ef6304550b45c30e02ea7ed8c2c497c
Alex Coupe <acoupe@gmail.com>
2019-08-18 14:57:15 +0100

    Better Nav Color Scheme

--- src/Components/Core/NavCol/NavItem.js
+++ src/Components/Core/NavCol/NavItem.js
@@ -7,8 +7,8 @@ export default function NavItem(props) {
             {props.links.map((element, i) => {   
                 return ( 
                 <React.Fragment key={i}>
-                    <a href="/" className=" btn btn-block btn-dark m-0" >{element}</a>
-                    <hr style={{margin: '0rem', borderColor: '#202020'}}/>
+                    <a href="/" className=" btn btn-block btn-secondary m-0" >{element}</a>
+                    <hr style={{margin: '0rem', borderColor: '#2F4F4F'}}/>
                 </React.Fragment>)
             })}
         </div>)
